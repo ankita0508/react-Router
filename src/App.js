2e8dcc51562ad6f8d70b79103936ed1d63f6bb94
ankita0508 <ankita_1994@outlook.com>
2021-03-07 12:17:39 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import "./style.css";
 import Nav from "./Nav";
 import Shop from "./Shop";
 import About from "./About";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div>
         <Nav />
-        <About />
-        <Shop />
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
       </div>
     </BrowserRouter>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
