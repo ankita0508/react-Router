2d2c6c8c87c7ec7b3fbda901f8996d32d94da1f5
ankita0508 <ankita_1994@outlook.com>
2021-03-07 14:52:34 +0530

    2 files modified

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,32 @@
 import React from "react";
 import "./style.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Shop() {
-  return <h1>Shop Page</h1>;
+  const [shopItems, setShopItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = () => {
+    let data = [];
+    axios
+      .get("https://fortnite-api.theapinetwork.com/upcoming/get")
+      .then(response => {
+        // console.log(response.data);
+        data = response.data;
+        //console.log(...data.data);
+        setShopItems(data.data);
+      });
+  };
+
+  return (
+    <div>
+      {shopItems.map(item => {
+        return <h1 key={item.itemId}>{item.item.name}</h1>;
+      })}
+    </div>
+  );
 }
